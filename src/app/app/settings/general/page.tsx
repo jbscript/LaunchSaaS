9838a3f23f1a47ce5e2be7adb1a58e72905ed667
Jabir Jaleel <jabir@maxstacklabs.com>
2025-03-12 17:13:18 +0530

    feat: implement GeneralUserForm and layout for general settings page

--- src/app/app/settings/general/page.tsx
+++ src/app/app/settings/general/page.tsx
@@ -1,8 +1,10 @@
+import { GeneralUserForm } from "@/components/settings/general/general-user-form";
 import { Separator } from "@/components/ui/separator";
 
 export default async function GeneralPage() {
   return (
     <div className="flex flex-col gap-8">
+      <GeneralUserForm defaultValues={{ name: "Documenso" }} />
       <Separator />
       <div className="flex flex-col gap-2">
         <p>Workspace Slug</p>
---
+++ src/components/settings/general/general-user-form.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+// or insertWorkspaceSchema.pick({ name: true }) and updating name to not be nullable
+const schema = z.object({
+  name: z.string().min(3, "workspace names must contain at least 3 characters"),
+});
+type Schema = z.infer<typeof schema>;
+
+export function GeneralUserForm({ defaultValues }: { defaultValues: Schema }) {
+  const form = useForm<Schema>({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  async function onSubmit(data: Schema) {
+    startTransition(async () => {
+      try {
+        console.log("data==>", data);
+        router.refresh();
+      } catch {
+        console.log("error");
+      }
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid w-full grid-cols-1 items-center gap-6 sm:grid-cols-6"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="sm:col-span-4">
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Documenso" {...field} />
+              </FormControl>
+              <FormDescription>The name of your workspace.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="sm:col-span-full">
+          <Button className="w-full sm:w-auto" size="lg">
+            {!isPending ? "Confirm" : "Loading..."}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
