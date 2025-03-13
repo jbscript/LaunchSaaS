f4413d1a82a86af56c7c08dd9ba5e858e8e230d5
Jabir Jaleel <jabir@maxstacklabs.com>
2025-03-13 11:10:25 +0530

    feat: add contact form with email sending functionality using nodemailer

---
+++ src/app/app/contact/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { sendMail } from "@/lib/send-mail";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+const contactFormSchema = z.object({
+  name: z.string().min(2, { message: "Please Enter Your Name" }),
+  email: z.string().email({ message: "Please Enter a Valid Email Address" }),
+  message: z.string().min(10, {
+    message: "Please make sure your message is at least 10 characters long.",
+  }),
+});
+export default function ContactForm() {
+  const form = useForm<z.infer<typeof contactFormSchema>>({
+    resolver: zodResolver(contactFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+  const isLoading = form.formState.isSubmitting;
+  const onSubmit = async (values: z.infer<typeof contactFormSchema>) => {
+    const mailText = `Name: ${values.name}\n  Email: ${values.email}\nMessage: ${values.message}`;
+    const response = await sendMail({
+      email: values.email,
+      subject: "New Contact Us Form",
+      text: mailText,
+    });
+    if (response?.messageId) {
+      alert("Application Submitted Successfully.");
+    } else {
+      alert("Failed To send application.");
+    }
+  };
+  return (
+    <Form {...form}>
+      <form
+        className="grid grid-cols-3 items-center p-4 lg:p-6"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="col-span-3 flex flex-col gap-4 lg:col-span-3 lg:gap-6">
+          <h2 className="lg:text-xl">Enter Your Good Name Here:</h2>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <h2 className="lg:text-xl">Enter Your Email Address:</h2>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="john@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <h2 className="lg:text-xl">Enter Your Message Here:</h2>
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="My question is which framework do you prefer to use?"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button disabled={isLoading}>
+            {isLoading ? "Sending....." : "Send"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
