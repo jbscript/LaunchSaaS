55b9d32e123775d318257d24624afcdefbb3f77c
Jabir Jaleel <jabir@maxstacklabs.com>
2025-03-20 22:13:54 +0530

    feat: implement admin layout and simplify admin page structure

---
+++ src/app/app/admin/layout.tsx
@@ -0,0 +1,27 @@
+import { Header } from "@/components/header/headers";
+import { Shell } from "@/components/header/shell";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export default function AdminLayout({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <Shell className="relative flex flex-1 flex-col overflow-x-hidden">
+      <Header
+        title="Admin"
+        description="Overview of all your data."
+        actions={<Button>+ Add</Button>}
+      />
+      <div
+        className={cn("flex h-full flex-1 flex-col gap-6 md:gap-8", className)}
+      >
+        {children}
+      </div>
+    </Shell>
+  );
+}
